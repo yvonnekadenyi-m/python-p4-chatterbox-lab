471d8f2fdc2de8d304a7c91c0abadb52bd8ceb33
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-16 15:11:07 +0300

    added to seed.py

--- server/seed.py
+++ server/seed.py
@@ -1,34 +1,17 @@
-#!/usr/bin/env python3
-
-from random import choice as rc
-
-from faker import Faker
 
 from app import app
 from models import db, Message
 
-fake = Faker()
+with app.app_context():  # <-- app context ensures SQLAlchemy knows the app
+    # Clear existing messages
+    db.session.query(Message).delete()
 
-usernames = [fake.first_name() for i in range(4)]
-if "Duane" not in usernames:
-    usernames.append("Duane")
-
-def make_messages():
-
-    Message.query.delete()
-    
-    messages = []
-
-    for i in range(20):
-        message = Message(
-            body=fake.sentence(),
-            username=rc(usernames),
-        )
-        messages.append(message)
+    # Add sample messages
+    messages = [
+        Message(body="Hello, World!", username="Ian"),
+        Message(body="Flask + React is fun!", username="Jacklyne")
+    ]
 
     db.session.add_all(messages)
-    db.session.commit()        
-
-if __name__ == '__main__':
-    with app.app_context():
-        make_messages()
+    db.session.commit()
+    print("Seeded the database!")
